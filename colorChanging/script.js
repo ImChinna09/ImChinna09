3893e3cac7dff1d7d8179e9b1bc123b65b96a1d0
ImChinna09 <143594659+ImChinna09@users.noreply.github.com>
2024-03-20 20:41:45 +0530

    ColorChange

---
+++ colorChanging/script.js
@@ -0,0 +1,5 @@
+
+function ChangeColor(){
+    document.body.style.backgroundColor = 'rgb(' + Math.round(Math.random() * 255) + ',' + Math.round(Math.random() *255) + ',' + Math.round(Math.random() * 255) +
+    ')'
+}
